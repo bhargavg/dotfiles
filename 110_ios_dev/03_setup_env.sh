fb88999d0fe43e00c0c36dd5960fde4919437aa9
Bhargav Gurlanka <bhargav.grlnk@gmail.com>
2018-04-24 11:53:37 +0700

    Install tools for iOS Dev

---
+++ 110_ios_dev/03_setup_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+### Install tools for ios development
+
+echo "====================iOS Dev Tools Start===================="
+
+# shellcheck source=/dev/null
+source ~/.rvm/scripts/rvm
+gem install cocoapods
+
+echo "=====================iOS Dev Tools End-===================="
